3464456c1b6fba3681a30a158598a0736e0e0c6d
Miller Ridgeway <miller.ridgeway@gmail.com>
2019-10-10 12:41:17 -0600

    Starting datastore client and overlay integration

--- src/main/java/datastore/DataStore.java
+++ src/main/java/datastore/DataStore.java
@@ -1,4 +1,26 @@
 package datastore;
 
-public interface DataStore {
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DataStore {
+    private String dir;
+    private ArrayList<String> files;
+    public DataStore(String dir){
+        this.dir = dir;
+        this.files = new ArrayList<>();
+    }
+
+    public void writeFile(String filename) throws IOException {
+
+    }
+
+    public String getDir(){
+        return this.dir;
+    }
+
+    public ArrayList<String> getFiles(){
+        return this.files;
+    }
+
 }
---
+++ src/main/java/peer/StoreClient.java
@@ -0,0 +1,9 @@
+package peer;
+
+import message.Message;
+import network.Connection;
+
+import java.io.IOException;
+
+public class StoreClient {
+}
