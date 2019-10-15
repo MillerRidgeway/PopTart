a3b3168db02bab82f8b1dadffdc8fdf55fbca40b
Miller Ridgeway <miller.ridgeway@gmail.com>
2019-10-15 09:13:30 -0600

    Adding startup script

---
+++ toaster.sh
@@ -0,0 +1,18 @@
+tab="--tab-with-profile=Miller"
+foo=""
+cmd="bash -c 'java -cp libs/PopTart.jar peer.DiscoveryPeer 51000'"
+
+mkdir /tmp/Peers/peerFiles{1..10}
+
+cd ~/Projects/PopTart/build/
+
+foo+=($tab -e "$cmd")
+gnome-terminal "${foo[@]}"
+
+for i in {1..10}
+  do
+    cmd="bash -c 'java -cp libs/PopTart.jar peer.MemberPeer 127.0.0.1 51000 /tmp/Peers/peerFiles${i}/'"
+    foo=($tab -e "$cmd")
+    gnome-terminal "${foo[@]}"
+  done
+
